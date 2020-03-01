0a26f82b487f28efb4d903014964a207341a53af
andressd1 <45497518+andressd1@users.noreply.github.com>
2020-03-01 16:54:49 +0000

    Implemented Omdb api for movie searches, but removed friend search temporarily whilst doing so. Displays movies based on searches by user and results from api. Also tracks their 20 most recent selected movies.

--- lib/Screens/SearchTab/searchpage.dart
+++ lib/Screens/SearchTab/searchpage.dart
@@ -3,6 +3,7 @@ import 'dart:convert';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:template/CustomView/BottomBar.dart';
+import 'package:http/http.dart' as http;
 
 final friendList = [
   "Elon Musk",
@@ -91,14 +92,13 @@ final movieList = [
 ];
 
 HashSet<String> friendHistorySet = new HashSet();
-HashSet<String> movieHistorySet = new HashSet();
+LinkedHashSet<MovieObject> movieHistorySet =
+new LinkedHashSet(); // history of clicks, keeping their MovieObjects
+Set data;
+String lastQuery = "zzz";
 
 class SearchPage extends StatefulWidget {
   final String title = "Search Page";
-  HashSet<String> movieElementSet = movieHistorySet;
-  final List<String> movieElementList = movieList;
-  HashSet<String> friendElementSet = friendHistorySet;
-  final List<String> friendElementList = friendList;
 
   @override
   _SearchPageState createState() => _SearchPageState();
@@ -116,20 +116,10 @@ class _SearchPageState extends State<SearchPage> {
             onPressed: () async {
               String queryJson = await showSearch(
                 context: context,
-                delegate: CustomSearchDelegate(
-                  widget.movieElementList,
-                  widget.friendElementList,
-                  widget.movieElementSet,
-                  widget.friendElementSet,
-                ),
+                delegate: CustomSearchDelegate(),
               );
-              SearchPageObject searchOb = SearchPageObject.fromJson(queryJson);
-
-              if (searchOb.isMovie) {
-                Navigator.pushNamed(context, '/moviepage');
-              } else {
-                Navigator.pushNamed(context, '/profile');
-              }
+              MovieObject movieOb = MovieObject.fromJson(queryJson);
+              Navigator.pushNamed(context, '/moviepage');
             },
           ),
         ],
@@ -139,19 +129,160 @@ class _SearchPageState extends State<SearchPage> {
   }
 }
 
+class MovieSearch {
+  Future getMovieData(String movieSearch) async {
+    if (movieSearch == lastQuery) {
+      return data;
+    }
+    lastQuery = movieSearch;
+    final String url =
+        "http://www.omdbapi.com/?apikey=80246e40&type=movie&page=1&s=" +
+            movieSearch.replaceAll(" ", "*") +
+            "*";
+    var res = await http.get(Uri.parse(url));
+    var resBody = json.decode(res.body);
+
+    if (resBody["Response"] == "False") {
+      if (resBody["Error"] == "Too many results.") {
+        return -1;
+      } else if (resBody["Error"] == "Movie not found!") {
+        return -2;
+      } else {
+        return -3;
+      }
+    }
+    List lData = resBody["Search"];
+    data = Set();
+
+    for (int i = 0; i < lData.length; i++) {
+      MovieObject m = new MovieObject(lData[i]["Title"], lData[i]["imdbID"],
+          lData[i]["Year"], lData[i]["Poster"]);
+      data.add(m);
+    }
+    return data;
+  }
+}
+
 class CustomSearchDelegate extends SearchDelegate<String> {
-  bool queryClickedMovie = false;
-  bool queryClickedFriend = false;
+  void addToMovieSet(MovieObject m) {
+    if (movieHistorySet.length >= 20) {
+      movieHistorySet.remove(movieHistorySet.remove(movieHistorySet.first));
+    }
+    movieHistorySet.add(m);
+  }
 
-  List<String> movieSuggestionList;
-  List<String> friendSuggestionList;
-  List<String> movieElementList;
-  HashSet<String> movieHistorySet;
-  List<String> friendElementList;
-  HashSet<String> friendHistorySet;
+  Widget createRecentSearch(BuildContext context) {
+    if (movieHistorySet.length == 0) {
+      return Center(
+        child: Container(
+          child: Text("Waiting for user input..."),
+        ),
+      );
+    } else {
+      return ListView.builder(
+        itemCount: movieHistorySet.length,
+        itemBuilder: (context, index) {
+          MovieObject m = movieHistorySet.elementAt(
+              movieHistorySet.length - 1 - index);
+          return ListTile(
+            title: Text(m.movieTitle, style: TextStyle(fontSize: 21)),
+            subtitle: Text(
+              m.movieYear,
+            ),
+            onTap: () {
+              close(context, m.toJson());
+            },
+            leading: Image.network(
+              m.movieImageURL,
+              height: 400,
+              width: 40,
+            ), //CircleAvatar(backgroundImage: NetworkImage(m.movieImageURL),),
+          );
+        },
+      );
+    }
+  }
 
-  CustomSearchDelegate(this.movieElementList, this.friendElementList,
-      this.movieHistorySet, this.friendHistorySet);
+  Widget createSearch(BuildContext context, String query) {
+    if (query == "" || query == null) {
+      return createRecentSearch(context);
+    }
+
+    else {
+      return FutureBuilder(
+        builder: (context, projectSnap) {
+          if (projectSnap.connectionState != ConnectionState.done) {
+            return Center(
+              child: Container(
+                child: Text("Loading..."),
+              ),
+            );
+          } else if (projectSnap.data == -1 ||
+              projectSnap.data == -2 ||
+              projectSnap.data == -3 ||
+              projectSnap.data == -4) {
+            String s;
+            bool recent = false;
+
+            switch (projectSnap.data) {
+              case -1:
+                {
+                  s = "Too many results. Please specify title more.";
+                  break;
+                }
+              case -2:
+                {
+                  s = "Movie with title \"" + query + "\" not found!";
+                  break;
+                }
+              case -3:
+                {
+                  s = "Error.";
+                  break;
+                }
+            }
+            if (recent) {
+              return createRecentSearch(context);
+            }
+            return Center(
+              child: Container(
+                child: Text(s),
+              ),
+            );
+          } else if (projectSnap.hasError) {
+            return Center(
+              child: Container(
+                child: Text("Error with query"),
+              ),
+            );
+          } else {
+            return ListView.builder(
+              itemCount: projectSnap.data.length,
+              itemBuilder: (context, index) {
+                MovieObject m = projectSnap.data.elementAt(index);
+                return ListTile(
+                  title: Text(m.movieTitle, style: TextStyle(fontSize: 21)),
+                  subtitle: Text(
+                    m.movieYear,
+                  ),
+                  onTap: () {
+                    addToMovieSet(m);
+                    close(context, m.toJson());
+                  },
+                  leading: Image.network(
+                    m.movieImageURL,
+                    height: 400,
+                    width: 40,
+                  ), //CircleAvatar(backgroundImage: NetworkImage(m.movieImageURL),),
+                );
+              },
+            );
+          }
+        },
+        future: MovieSearch().getMovieData(query),
+      );
+    }
+  }
 
   @override
   List<Widget> buildActions(BuildContext context) {
@@ -177,98 +308,104 @@ class CustomSearchDelegate extends SearchDelegate<String> {
 
   @override
   Widget buildResults(BuildContext context) {
-    return createSearchScreen(context);
+    return createSearch(context, query);
   }
 
   @override
   Widget buildSuggestions(BuildContext context) {
-    return createSearchScreen(context);
+    return createSearch(context, query);
   }
 
   Widget createSearchScreen(BuildContext context) {
-    movieSuggestionList = query.isEmpty
-        ? movieHistorySet.toList()
-        : movieElementList
-        .where((m) => m.toLowerCase().startsWith(query.toLowerCase()))
-        .toList();
-    int msLength = movieSuggestionList.length;
-
-    friendSuggestionList = query.isEmpty
-        ? friendHistorySet.toList()
-        : friendElementList
-        .where((m) => m.toLowerCase().startsWith(query.toLowerCase()))
-        .toList();
-
-    int fsLength = friendSuggestionList.length;
-
-    SearchPageObject o;
-
-    if (msLength + fsLength >= 1) {
-      return ListView.builder(
-        itemBuilder: (context, index) {
-          if (index == 0 || index == msLength + 1) {
-            bool x = (index > 0) ? (fsLength > 0) : (msLength > 0);
-            return (x
-                ? ListTile(
-              title: Text(((index > 0) ? "People" : "Movies"),
-                  style: TextStyle(fontSize: 21)),
-            )
-                : SizedBox(
-              height: 1,
-            ));
-          } else if (index < 1 + msLength) {
-            o = new SearchPageObject(
-                true, movieSuggestionList[index - 1], Icon(Icons.movie));
-          } else {
-            o = new SearchPageObject(false,
-                friendSuggestionList[index - msLength - 2], Icon(Icons.people));
-          }
-          return ListTile(
-              leading: o.icon,
-              title: Text(
-                o.queryString,
-                style: TextStyle(fontSize: 18),
-              ),
-              onTap: () {
-                query = o.queryString;
-                (o.isMovie)
-                    ? movieHistorySet.add(query)
-                    : friendHistorySet.add(query);
-                close(context, o.toJson());
-              });
-        },
-        itemCount: 2 + msLength + fsLength,
-      );
-    } else {
-      return ListTile(
-        title: Text(
-          "No results",
-          textAlign: TextAlign.center,
-        ),
-      );
-    }
+//    movieSuggestionList = query.isEmpty
+//        ? movieHistorySet.toList()
+//        : movieElementList
+//            .where((m) => m.toLowerCase().startsWith(query.toLowerCase()))
+//            .toList();
+//    int msLength = movieSuggestionList.length;
+//
+//    friendSuggestionList = query.isEmpty
+//        ? friendHistorySet.toList()
+//        : friendElementList
+//            .where((m) => m.toLowerCase().startsWith(query.toLowerCase()))
+//            .toList();
+//
+//    int fsLength = friendSuggestionList.length;
+//
+//    SearchPageObject o;
+//
+//    if (msLength + fsLength >= 1) {
+//      return ListView.builder(
+//        itemBuilder: (context, index) {
+//          if (index == 0 || index == msLength + 1) {
+//            bool x = (index > 0) ? (fsLength > 0) : (msLength > 0);
+//            return (x
+//                ? ListTile(
+//                    title: Text(((index > 0) ? "People" : "Movies"),
+//                        style: TextStyle(fontSize: 21)),
+//                  )
+//                : SizedBox(
+//                    height: 1,
+//                  ));
+//          } else if (index < 1 + msLength) {
+//            o = new SearchPageObject(
+//                true, movieSuggestionList[index - 1], Icon(Icons.movie));
+//          } else {
+//            o = new SearchPageObject(false,
+//                friendSuggestionList[index - msLength - 2], Icon(Icons.people));
+//          }
+//          return ListTile(
+//              leading: o.icon,
+//              title: Text(
+//                o.queryString,
+//                style: TextStyle(fontSize: 18),
+//              ),
+//              onTap: () {
+//                query = o.queryString;
+//                (o.isMovie)
+//                    ? movieHistorySet.add(query)
+//                    : friendHistorySet.add(query);
+//                close(context, o.toJson());
+//              });
+//        },
+//        itemCount: 2 + msLength + fsLength,
+//      );
+//    } else {
+//      return ListTile(
+//        title: Text(
+//          "No results",
+//          textAlign: TextAlign.center,
+//        ),
+//      );
+//    }
   }
 }
 
-class SearchPageObject {
-  bool isMovie;
-  String queryString;
-  Icon icon;
+class MovieObject {
+  String movieTitle;
+  String movieID;
+  String movieYear;
+  String movieImageURL;
 
-  SearchPageObject(bool s, String queryS, Icon i) {
-    isMovie = s;
-    queryString = queryS;
-    icon = i;
+  MovieObject(mTitle, mID, mYear, mImage) {
+    this.movieTitle = mTitle;
+    this.movieID = mID;
+    this.movieYear = mYear;
+    this.movieImageURL = mImage;
   }
 
   String toJson() {
-    return json
-        .encode({"isMovie": this.isMovie, "querryString": this.queryString});
+    return json.encode({
+      "movieTitle": this.movieTitle,
+      "movieID": this.movieID,
+      "movieYear": this.movieYear,
+      "movieImageURL": this.movieImageURL
+    });
   }
 
-  static SearchPageObject fromJson(String maps) {
+  static MovieObject fromJson(String maps) {
     var result = json.decode(maps);
-    return SearchPageObject(
-        result["isMovie"], result["querryString"], Icon(Icons.title));
+    return MovieObject(result["movieTitle"], result["movieID"],
+        result["movieYear"], result["movieImageURL"]);
   }
 }
